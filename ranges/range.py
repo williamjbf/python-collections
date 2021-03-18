1c0971982af911fd8ccba5ae6c08a10d66babd33
williamjbf <williamjbf@gmail.com>
2021-03-18 16:10:38 -0300

    range

---
+++ ranges/range.py
@@ -0,0 +1,17 @@
+range1 = range(5)
+range2 = range(2,10)
+range3 = range(2,10,2)
+
+print(range1)
+for i in range1:
+    print(i)
+print(30*'-')
+print(range2)
+for i in range2:
+    print(i)
+print(30*'-')
+print(range3)
+for i in range3:
+    print(i)
+
+    
