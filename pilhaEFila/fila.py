d136d036caa2aa99c48d913e9d060217c4ec51a1
williamjbf <williamjbf@gmail.com>
2021-03-18 19:13:41 -0300

    add Pilha e Fila

---
+++ pilhaEFila/fila.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+minhaFila = deque(["João","Pedro","Augusto"])
+print(minhaFila)
+minhaFila.popleft()
+print(minhaFila)
+
+minhaFila.append("Maria")
+minhaFila.popleft
+print(minhaFila)
---
+++ pilhaEFila/pilha.py
@@ -0,0 +1,7 @@
+minhaPilha = [1,2,3,4]
+
+minhaPilha.append(5)
+
+minhaPilha.pop()
+minhaPilha.pop()
+print(minhaPilha)
